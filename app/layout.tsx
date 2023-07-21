dfc0883e7c31fb52e5a738ac1b6b29e733e35e3a
Francis Ngo <francis.t.ngo@gmail.com>
2023-07-21 11:45:48 -0700

    Build navbar ui

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+
+import Navbar from "./components/navbar/Navbar";
+
+export const metadata: Metadata = {
+	title: "Campbnb",
+	description:
+		"Find the perfect place to camp at an amazing price. Go outdoors with Campbnb!",
+};
+
+const font = Manrope({
+	subsets: ["latin"],
+});
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={font.className}>
+				<Navbar />
+				{children}
+			</body>
+		</html>
+	);
+}
