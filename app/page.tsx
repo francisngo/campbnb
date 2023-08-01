b861e2de35355b5a6e5a3899d5bed0986cc9c171
Francis Ngo <francis.t.ngo@gmail.com>
2023-08-01 11:43:52 -0700

    Create ListingCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,42 @@
 import ClientComponent from "./components/ClientComponent";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
+import Container from "@/app/components/Container";
+import EmptyState from "@/app/components/EmptyState";
+import ListingCard from "@/app/components/listings/ListingCard";
 
-export default function Home() {
-	const isEmpty = true;
-	if (isEmpty) {
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings, { IListingsParams } from "./actions/getListings";
+
+interface HomeProps {
+	searchParams: IListingsParams;
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+	const listings = await getListings(searchParams);
+	const currentUser = await getCurrentUser();
+
+	if (listings.length === 0) {
 		return (
 			<ClientComponent>
-				<EmptyState />
+				<EmptyState showReset />
 			</ClientComponent>
 		);
 	}
+
 	return (
 		<ClientComponent>
 			<Container>
 				<div className="pt-24 grid grid-cols sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-					<div>My Future Listings</div>
+					{listings.map((listing: any) => (
+						<ListingCard
+							currentUser={currentUser}
+							key={listing.id}
+							data={listing}
+						/>
+					))}
 				</div>
 			</Container>
 		</ClientComponent>
 	);
-}
+};
+
+export default Home;
